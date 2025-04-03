9f0ae3a95547ca8398e248321971c86422ad912b
Izhar Ahmed <izharahmed1280@gmail.com>
2025-04-03 14:47:31 +0530

    some correction

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,4 @@
-const Product = require("../models/Product");
-const { findByIdAndUpdate } = require("../models/User");
+const Product = require("../models/Product")
 
 const createProduct = async(req, res)=> {
     try {
